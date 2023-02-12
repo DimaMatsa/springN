d503ab52fd2268ed206ffeeaa94304060212d339
Dima <wumf@ukr.net>
2023-02-12 16:47:40 +0200

    init prog

---
+++ src/test/java/com/matsa/SpringNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
