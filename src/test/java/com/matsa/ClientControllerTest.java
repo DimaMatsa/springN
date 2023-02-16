5287717a669155b4dd7e480e2753efa3197e14ca
Dima <wumf@ukr.net>
2023-02-16 23:45:42 +0200

    tests

--- src/test/java/com/matsa/ClientControllerTest.java
+++ src/test/java/com/matsa/ClientControllerTest.java
@@ -128,7 +128,7 @@ public class ClientControllerTest {
     }
 
     @Test
-    void findClient() throws Exception {
+    void findClients() throws Exception {
         ClientDto testClient = mockClient();
         String json = objectMapper.writeValueAsString(testClient);
         mockMvc.perform(post("/client").contentType(MediaType.APPLICATION_JSON).content(json))
